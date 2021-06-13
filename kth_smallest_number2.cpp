eae4d5830c8d0fb8cf72121d205b490cc8101e0e
NewmanAdesan <83893239+NewmanAdesan@users.noreply.github.com>
2021-06-13 21:24:52 +0000

    Update kth_smallest_number2.cpp

--- kth_smallest_number2.cpp
+++ kth_smallest_number2.cpp
@@ -2,44 +2,46 @@
 //Initial Template for C
 
 /***********************************************************************************************
- *   Problem :  Kth Smallest Number                                                            
- *   
- *   Info : Given an array arr[] and a number K where K is smaller than size of array,
- *          the task is to find the Kth smallest element in the given array. 
- *          It is given that all array elements are distinct.
- * 
- *   Example :  Input:
- *              N = 6
- *              arr[] = 7 10 4 3 20 15
- *              K = 3
- *              Output : 7
- *              Explanation :
- *              3rd smallest element in the given 
- *              array is 7. 
- * 
- *   Time Complexity : O(n)
- *   Space Complexity : O(1)
- * 
- *   
- * -------------------------------------------------------------------------------------------
- * ***********************************   MY PLAN   *******************************************
- * it actually simple i call this technique the Modulo Technique
- * a technique that helps with consistent looping without 
- * increasing the complexity.
- * 
- * For Instance: if given K = 2 --- 2nd smallest
- * what i do is loop through the array to look for the smallest element, when found
- * replace that element with the maximun element, then loop through the array again
- * to find the 2nd Smallest element.
- * This is done with just one While loop......
- * 
- * if 5 elements in array, i would actually loop ten times. i goes from 0 - 9
- * but with the help of modulo(%) it will be 0-4 and another 0-4
- * which micmicks looping twice all done with one loop
- * 
- * meaning if k= 3, n = 10.......while loop i goes from 0 - k*n
- * modulo makes it  0-9, 0-9, 0-9
- * 
+ *   Problem :  Kth Smallest Number                                                            *
+ *                                                                                             *
+ *   Problem Solving Technique Used : Modulo Technique                                         *
+ *                                                                                             *
+ *   Info : Given an array arr[] and a number K where K is smaller than size of array,         *
+ *          the task is to find the Kth smallest element in the given array.                   *
+ *          It is given that all array elements are distinct.                                  *
+ *                                                                                             *
+ *   Example :  Input:                                                                         *
+ *              N = 6                                                                          *
+ *              arr[] = 7 10 4 3 20 15                                                         *
+ *              K = 3                                                                          *
+ *              Output : 7                                                                     *
+ *              Explanation :                                                                  *
+ *              3rd smallest element in the given                                              *
+ *              array is 7.                                                                    *
+ *                                                                                             *
+ *   Time Complexity : O(n)                                                                    *
+ *   Space Complexity : O(1)                                                                   *
+ *                                                                                             *
+ *                                                                                             *
+ * ---------------------------------------------------------------------------------------------
+ * ***********************************   MY PLAN   *********************************************
+ * it actually simple i call this technique the Modulo Technique                               *
+ * a technique that helps with consistent looping without                                      *
+ * increasing the complexity.                                                                  *
+ *                                                                                             *
+ * For Instance: if given K = 2 --- 2nd smallest                                               *
+ * what i do is loop through the array to look for the smallest element, when found            *
+ * replace that element with the maximun element, then loop through the array again            *
+ * to find the 2nd Smallest element.                                                           *
+ * This is done with just one While loop......                                                 *
+ *                                                                                             *
+ * if 5 elements in array, i would actually loop ten times. i goes from 0 - 9                  *
+ * but with the help of modulo(%) it will be 0-4 and another 0-4                               *
+ * which micmicks looping twice all done with one loop                                         *
+ *                                                                                             *
+ * meaning if k= 3, n = 10.......while loop i goes from 0 - k*n                                *
+ * modulo makes it  0-9, 0-9, 0-9                                                              *
+ *                                                                                             *
  ***********************************************************************************************/
 
 
