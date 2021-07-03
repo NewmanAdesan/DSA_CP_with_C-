99d7830fda59b657644c58418e46dd5fc1147154
NewmanAdesan <83893239+NewmanAdesan@users.noreply.github.com>
2021-07-03 22:14:13 +0000

    Add files via upload

---
+++ Rotate_by_90_degrees_O(n)_solution.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+
+//Function to rotate matrix anticlockwise by 90 degrees.
+void rotateby90(int n, int matrix[][n]) 
+{
+    // the key technique is just
+    // simple recognition
+    // simple reverse
+    
+    //creation of the extra space
+    int matrix2[n][n];
+
+    for (int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            matrix2[n-1-j][i] = matrix[i][j];
+        }
+    }
+    
+    for (int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            matrix[i][j] = matrix2[i][j];
+        }
+    }
+    
+    
+    
+    
+} 
+
+// { Driver Code Starts.
+
+int main()
+{
+    int tc;
+	scanf("%d", &tc);
+	while(tc--){
+		int n;
+		scanf("%d", &n);
+		int matrix[n][n];
+	 
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				scanf("%d", &matrix[i][j]);
+			}
+		}
+		
+		rotateby90(n, matrix);
+		for(int i = 0; i < n; i++)
+		    for(int j=0; j<n; j++)
+			    printf("%d ", matrix[i][j]);
+		printf("\n");
+	}
+	return 0;
+}  // } Driver Code Ends
